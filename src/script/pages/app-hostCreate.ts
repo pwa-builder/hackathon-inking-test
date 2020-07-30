48edc465f18de413ae6e43f45b6258c3bcf2e899
Rumsha <rumsha7@gmail.com>
2020-07-29 19:27:03 -0700

    design generated code page

--- src/script/pages/app-hostCreate.ts
+++ src/script/pages/app-hostCreate.ts
@@ -8,67 +8,51 @@ export class AppHostCreate extends LitElement {
   static get styles() {
     return css`
     #main {
-      text-align: center;
-      position: absolute;
-      width: 100%;
-      height: 100%;
-      left: 0px;
-      top: 0px;
-      background: linear-gradient(180deg, #FFFFFF 0%, rgba(255, 255, 255, 0) 100%), url(assets/images/background.svg);
-  }
+        text-align: center;
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        left: 0px;
+        top: 0px;
+        background: linear-gradient(180deg, #FFFFFF 0%, rgba(255, 255, 255, 0) 100%), url(assets/images/background.svg);
+    }
 
-  #greeting {
-    position: absolute;
-    width: 100%;
-    height: 100%;
-    
-    font-family: Roboto;
-    font-style: normal;
-    font-weight: bold;
-    font-size: 60px;
-    line-height: 70px;
-    
-    /* Gray 1 */
-    color: #333333; 
-  }
+    #greeting {
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        
+        font-family: Roboto;
+        font-style: normal;
+        font-weight: bold;
+        font-size: 60px;
+        line-height: 70px;
+        
+        /* Gray 1 */
+        color: #333333; 
+    }
 
-  #logo {      
-    margin: 0;
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    margin-right: -50%;
-    transform: translate(-50%, -50%);
-  }
-     
-  #hostOptionCreate {
-    margin: 0;
-    position: absolute;
-    top: 35%;
-    left: 50%;
-    margin-right: -50%;
-    transform: translate(-50%, -50%);
-    background-image: url('assets/images/button_create.svg');
-  }
+    #code {      
+        margin: 0;
+        position: absolute;
+        top: 35%;
+        left: 50%;
+        margin-right: -50%;
+        transform: translate(-50%, -50%);
+        font-size: 40px; 
+        border-width: 50px;
+        border-style: solid;
+        border-image: url('assets/images/code_generated.svg') 25% round;
+    }    
 
-  #hostOptionCalendar {
-    margin: 0;
-    position: absolute;
-    top: 55%;
-    left: 50%;
-    margin-right: -50%;
-    transform: translate(-50%, -50%);
-  }
-
-  #hostOptionCreate:hover {
-    background-image: url('assets/images/button_create_pressed.svg');
-  }
-
-  #hostOptionCalendar:hover {
-    background-image: url('assets/images/button_calendar_pressed.svg');
-  }
-
-    
+    #start {
+        margin: 0;
+        position: absolute;
+        top: 60%;
+        left: 50%;
+        margin-right: -50%;
+        transform: translate(-50%, -50%);
+    }
     `;
   }
 
@@ -80,8 +64,10 @@ export class AppHostCreate extends LitElement {
     return html`
       <div id="main">
       <h3 id="greeting">${greetinghostcreate}</h3>
-      <img src="assets/images/code_generated.svg">
+      <h3 id="code">cu$t0m c0de</h3>
+      <div id="start">
       <a href="./play"><img src="assets/images/button_start.svg"></a>
+      </div>
       </div>
     `;
   }
